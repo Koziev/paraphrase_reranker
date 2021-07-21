d27f793baa227660c8dd005012e8f46ec1c8d42d
koziev ilya <kelijah@yandex.ru>
2021-07-21 07:15:30 +0300

    еще часть кода первого релиза

---
+++ setup.py
@@ -0,0 +1,14 @@
+import io
+import setuptools
+
+setuptools.setup(
+    name="paraphrase_reranker",
+    version="0.0.1",
+    author="Ilya Koziev",
+    author_email="inkoziev@gmail.com",
+    description="Paraphrase detection and reranking",
+    url="https://github.com/Koziev/paraphrase_reranker",
+    packages=setuptools.find_packages(),
+    package_data={'paraphrase_reranker': ['*.py', '*.cfg', '*.pt']},
+    include_package_data=True,
+)
